0f45ac5ff51c490b7ee6266732e1ea9307aabe65
Alli Gilbreath <alligilbreath@dhcp-10-132-129-166.uawifi.arizona.edu>
2018-03-29 14:12:55 -0700

    See if it's the sorting that's causing a compiler error

--- src/chars.cpp
+++ src/chars.cpp
@@ -164,7 +164,7 @@ void Chars::WriteCardsToFile() {
         cout << "Could not open " << _outputFilePath << endl;
     }
     //first sort all of the cards
-    std::sort(_cards.begin(), _cards.end());
+   // std::sort(_cards.begin(), _cards.end());
     //then output to file
     std::list<Card>::iterator card = _cards.begin();// Create an iterator object
     while(card != _cards.end()){ // keep iterating until we hit the end
